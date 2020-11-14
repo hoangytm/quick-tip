c3932446331f68f6205e185e9f046a2685764d6c
Hoang Phan <51194076+hoangytm@users.noreply.github.com>
2020-11-14 22:48:16 +0700

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+function percentChange(val) {
+   document.getElementById('tipPercent').innerHTML = val + "%";
+}
+
+function personChange(val) {
+   document.getElementById('person').innerHTML = val + " persons";
+}
